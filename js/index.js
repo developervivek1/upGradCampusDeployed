2a885f3e305a044e746c36d984f3916ed5b68bd4
Chethan-Goprotoz <chethan@goprotoz.com>
2022-07-13 18:39:03 +0530

    home Page Card fixed

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
-
 //owl carouse
 $(document).ready(function () {
-  $('.owl-one').owlCarousel({
+  $(".owl-one").owlCarousel({
     center: true,
     items: 4,
     loop: true,
-    autoplay:true,
+    autoplay: true,
     autoplayTimeout: 2500,
     dots: false,
     autoplayHoverPause: true,
     margin: 20,
     smartSpeed: 500,
+    autoHeight: true,
     responsive: {
       0: {
         stagePadding: 20,
-        items: 1
+        items: 1,
       },
       600: {
         items: 2,
-        autoplay:false,
+        autoplay: false,
       },
-      700: {
-        items: 3,
-        autoplay:false,
-      },
+
       1024: {
+        items: 3,
+      },
+      1300: {
         items: 4,
-      }
-    }
+      },
+    },
   });
-  $('.owl-two').owlCarousel({
+  $(".owl-two").owlCarousel({
     items: 6,
     loop: true,
     margin: 30,
@@ -39,39 +39,38 @@ $(document).ready(function () {
     autoplayTimeout: 1500,
     autoplayHoverPause: true,
     smartSpeed: 1500,
-    animateIn: 'linear',
-    animateOut: 'linear',
+    animateIn: "linear",
+    animateOut: "linear",
     responsive: {
       0: {
-        items: 3
+        items: 3,
       },
       600: {
-        items: 2
+        items: 2,
       },
       1000: {
-        items: 6
-      }
-    }
+        items: 6,
+      },
+    },
   });
 });
 
 // counter
 const counters = document.querySelectorAll(".count");
-  const speed = 50;
-  const speed2 = 2;
-  counters.forEach((counter) => {
-    const updateCount = () => {
-      const target = parseInt(+counter.getAttribute("data-target"));
-      const count = parseInt(+counter.innerText);
-      const increment = Math.trunc(target / speed);
-      const increment2 = Math.trunc(target / speed2);
-      if (count < target) {
-        counter.innerText = count + increment;
-        setTimeout(updateCount, 1);
-      }
-      else {
-        count.innerText = target;
-      }
-    };
-    updateCount();
-  });
+const speed = 50;
+const speed2 = 2;
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = parseInt(+counter.getAttribute("data-target"));
+    const count = parseInt(+counter.innerText);
+    const increment = Math.trunc(target / speed);
+    const increment2 = Math.trunc(target / speed2);
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
--- js/popmodal.js
+++ js/popmodal.js
@@ -1,1 +1,45 @@
-const wapp=document.querySelector(".whatsapp"),allImg=document.querySelectorAll("img");allImg.forEach(a=>{a.setAttribute("loading","lazy")}),window.addEventListener("DOMContentLoaded",()=>{setTimeout(()=>{wapp.classList.contains("active")||wapp.classList.add("active")},5e3),setTimeout(()=>{wapp.classList.contains("active")&&wapp.classList.remove("active")},3e5)});let setOfevents=["mousemove","mousedown","scroll","click","dblclick","mouseleave","mouseenter","resize"],setTimeforPop;function detecttype(a){setOfevents.includes(a.type)&&(clearTimeout(setTimeforPop),setTimeforPop=setTimeout(dispSubscribe,15e3))}function dispSubscribe(){let a=document.querySelector("#subscribeModal"),b=document.querySelector(".overlay");a.classList.add("show"),b.classList.add("active"),a.addEventListener("click",()=>{a.classList.contains("show")&&(a.classList.remove("show"),b.classList.remove("active"))})}setOfevents.forEach(a=>{window.addEventListener(a,detecttype)}),window.addEventListener("DOMContentLoaded",()=>{setTimeout(dispSubscribe,15e3)})
+const wapp = document.querySelector(".whatsapp"),
+  allImg = document.querySelectorAll("img");
+allImg.forEach((a) => {
+  a.setAttribute("loading", "lazy");
+}),
+  window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+      wapp.classList.contains("active") || wapp.classList.add("active");
+    }, 5e3),
+      setTimeout(() => {
+        wapp.classList.contains("active") && wapp.classList.remove("active");
+      }, 3e5);
+  });
+let setOfevents = [
+    "mousemove",
+    "mousedown",
+    "scroll",
+    "click",
+    "dblclick",
+    "mouseleave",
+    "mouseenter",
+    "resize",
+  ],
+  setTimeforPop;
+function detecttype(a) {
+  setOfevents.includes(a.type) &&
+    (clearTimeout(setTimeforPop),
+    (setTimeforPop = setTimeout(dispSubscribe, 20e3)));
+}
+function dispSubscribe() {
+  let a = document.querySelector("#subscribeModal"),
+    b = document.querySelector(".overlay");
+  a.classList.add("show"),
+    b.classList.add("active"),
+    a.addEventListener("click", () => {
+      a.classList.contains("show") &&
+        (a.classList.remove("show"), b.classList.remove("active"));
+    });
+}
+setOfevents.forEach((a) => {
+  window.addEventListener(a, detecttype);
+}),
+  window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(dispSubscribe, 20e3);
+  });
